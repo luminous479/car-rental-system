7a59ecdb41de0329c3115ffa9ce796f3a26fe0fa
luminous479 <aklemasultanakhi@gmail.com>
2026-09-17 06:23:16 +0600

    feat: add insurance service to sequential API calls

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,50 +1,65 @@
 package main
 
 import (
-	"encoding/json"
+	
 	"fmt"
-	"io"
-	"net/http"
+	
 	"time"
 )
 
 type CarInfo struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	Name string 
 }
+type Availability struct {
+  Available bool
+
+}
+type Pricing struct{ PricePerDay float64 }
+type Reviews struct{ AverageRating float64 }
+type InsuranceQuote struct{ Quote float64 }
+
+func getCarInfo(carID string) CarInfo {
+	time.Sleep(1 * time.Second) 
+	return CarInfo{Name: "Toyota Corolla"}
+}
+
+func getAvailability(carID string) Availability {
+	time.Sleep(1 * time.Second)
+	return Availability{Available: true}
+}
+
+func getPricing(carID string) Pricing {
+	time.Sleep(1 * time.Second)
+	return Pricing{PricePerDay: 35.0}
+}
+
+func getReviews(carID string) Reviews {
+	time.Sleep(1 * time.Second)
+	return Reviews{AverageRating: 4.5}
+}
+
+func getInsuranceQuote(carID string) InsuranceQuote {
+	time.Sleep(1 * time.Second)
+	return InsuranceQuote{Quote: 15.0}
+}
+
 
 func main() {
 
-	client := &http.Client{}
+	start := time.Now()
+	carID := "car-123"
+	
+	carInfo := getCarInfo(carID)
+	availability := getAvailability(carID)
+	pricing := getPricing(carID)
+	reviews := getReviews(carID)
+	insuranceQuote := getInsuranceQuote(carID)
 
-	fmt.Println("Before request:", time.Now())
-
-	res, err := client.Get("https://jsonplaceholder.typicode.com/users/2")
-
-	fmt.Println("After request:", time.Now())
-
-	if err != nil {
-		fmt.Println("request failed", err)
-		return
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		fmt.Println("request failed with status", res.Status)
-		return
-	}
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println("failed to read response body", err)
-		return
-	}
-
-	var carInfo CarInfo
-	err = json.Unmarshal(body, &carInfo)
-	if err != nil {
-		fmt.Println("failed to unmarshal response body", err)
-		return
-	}
-
+	elapsed := time.Since(start)
 	fmt.Printf("Car Info: %+v\n", carInfo)
+	fmt.Printf("Availability: %+v\n", availability)
+	fmt.Printf("Pricing: %+v\n", pricing)
+	fmt.Printf("Reviews: %+v\n", reviews)
+	fmt.Printf("Insurance Quote: %+v\n", insuranceQuote)
+	fmt.Printf("Time taken: %s\n", elapsed)
 }
